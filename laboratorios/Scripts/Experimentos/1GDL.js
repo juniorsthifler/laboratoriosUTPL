d4cf7d79acf473dc26ea3a8185d92cd0130db725
jbvasquez1 <jbvasquez1@utpl.edu.ec>
2017-06-15 19:52:36 -0500

    nuevo archivo de javascript

---
+++ laboratorios/Scripts/Experimentos/1GDL.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+
+    $('#ibtnLoadExample').click(function () {
+        console.log("Cargando ejemplo.");
+        $('#txtT').val("10");
+        $('#txtM').val("9");
+        $('#txtA').val("5000");
+
+
+
+
+    });
+});
